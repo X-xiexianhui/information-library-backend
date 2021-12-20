63a033a22640e22f7b0161456eaa9b7ecff66608
xiexianhui <xiexianhuicn@outlook.com>
2021-12-20 15:24:14 +0800

    init

---
+++ src/main/java/com/gxu/informationLibrarySystem/InformationLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.gxu.informationLibrarySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationLibrarySystemApplication.class, args);
+	}
+
+}
