3fe74cfd8cd14975c8e24596164d6f3ef618ee2f
xiexianhui <xiexianhuicn@outlook.com>
2022-03-24 22:27:56 +0800

    "创建数据表"

--- src/main/java/com/gxu/informationLibrary/entity/column.java
+++ src/main/java/com/gxu/informationLibrary/entity/column.java
@@ -11,10 +11,10 @@ public class column {
     String tbName;
     String colName;
     String datatype;
-    int length;
+    int len;
     int place;
     boolean PK;
-    boolean unique;
+    boolean uni;
     boolean not_null;
 
     public column(JSONObject jsonObject,String dbName,String tbName) {
@@ -22,10 +22,10 @@ public class column {
         this.tbName=tbName;
         this.colName = jsonObject.getString("colName");
         this.datatype = jsonObject.getString("datatype");
-        this.length = jsonObject.getInt("length");
+        this.len = jsonObject.getInt("length");
         this.place = jsonObject.getInt("place");
         this.PK = jsonObject.getBoolean("PK");
-        this.unique = jsonObject.getBoolean("unique");
+        this.uni = jsonObject.getBoolean("unique");
         this.not_null = jsonObject.getBoolean("not_null");
     }
 }
