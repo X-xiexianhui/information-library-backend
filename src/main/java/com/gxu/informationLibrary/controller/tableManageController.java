8c6fe095912c7dc9ec945cbeb8c1d00e2fc66295
xiexianhui <xiexianhuicn@outlook.com>
2022-03-24 16:32:11 +0800

    "创建数据表"

--- src/main/java/com/gxu/informationLibrary/controller/tableManageController.java
+++ src/main/java/com/gxu/informationLibrary/controller/tableManageController.java
@@ -1,17 +1,12 @@
 package com.gxu.informationLibrary.controller;
 
-import com.gxu.informationLibrary.entity.Columns;
-import com.gxu.informationLibrary.entity.column;
 import com.gxu.informationLibrary.entity.response;
 import com.gxu.informationLibrary.serviceImpl.tbManageImpl;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 
 
 @Controller
--- src/main/java/com/gxu/informationLibrary/server/tbManageServer.java
+++ src/main/java/com/gxu/informationLibrary/server/tbManageServer.java
@@ -1,9 +1,5 @@
 package com.gxu.informationLibrary.server;
 
-import com.gxu.informationLibrary.entity.column;
-
-import java.util.List;
-
 public interface tbManageServer {
     void createTable(String Param);
 }
