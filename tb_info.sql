4e74ada38c5f557a3aa4de1db61f13ece452f489
xiexianhui <xiexianhuicn@outlook.com>
2022-03-28 17:01:32 +0800

    "init database"

--- tb_info.sql
+++ tb_info.sql
@@ -2,7 +2,8 @@ create table tb_info
 (
     tb_id   int auto_increment primary key,
     db_name varchar(64) not null,
-    tb_name varchar(64) not null
+    tb_name varchar(64) not null,
+    constraint tb_info_tb_name_db_info_db_name foreign key (db_name) references db_info(db_name)
 );
 
 
