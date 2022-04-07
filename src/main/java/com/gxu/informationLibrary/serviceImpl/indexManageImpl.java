03d1ca5c843f458db28041cd99cd75fd750b2743
xiexianhui <xiexianhuicn@outlook.com>
2022-04-07 20:38:28 +0800

    "alter index"

--- src/main/java/com/gxu/informationLibrary/serviceImpl/indexManageImpl.java
+++ src/main/java/com/gxu/informationLibrary/serviceImpl/indexManageImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.gxu.informationLibrary.dao.indexManageDao;
 import com.gxu.informationLibrary.entity.index;
+import com.gxu.informationLibrary.entity.indexList;
 import com.gxu.informationLibrary.server.indexManage;
 import org.springframework.stereotype.Service;
 
@@ -20,20 +21,22 @@ public class indexManageImpl implements indexManage {
     @Override
     public List<index> alterIndex(String Parma) {
         JSONObject indexJson = JSON.parseObject(Parma);
-
-        addIndex();
-        deleteIndex();
-        updateIndex();
+        List<index>insert=new indexList(indexJson.getJSONArray("insert")).getIndexes();
+        List<index>remove=new indexList(indexJson.getJSONArray("remove")).getIndexes();
+        List<index>update=new indexList(indexJson.getJSONArray("update")).getIndexes();
+        addIndex(insert);
+        deleteIndex(remove);
+        updateIndex(update);
         return getIndex();
     }
 
-    private void addIndex() {
+    private void addIndex(List<index>insert) {
 
     }
-    private void deleteIndex(){
+    private void deleteIndex(List<index>remove){
 
     }
-    private void updateIndex(){
+    private void updateIndex(List<index>update){
 
     }
     private List<index> getIndex(){
