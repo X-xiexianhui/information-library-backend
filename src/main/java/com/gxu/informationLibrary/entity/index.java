ef0ec2a85a850e2fcf1fb87c4a7f7b0ac866fa20
xiexianhui <xiexianhuicn@outlook.com>
2022-04-07 17:01:04 +0800

    "alter index"

--- src/main/java/com/gxu/informationLibrary/entity/index.java
+++ src/main/java/com/gxu/informationLibrary/entity/index.java
@@ -4,21 +4,26 @@ import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Getter
 @Setter
 public class index {
+    String db_name;
+    String tb_name;
     List<String>fields;
     Boolean uni;
 
-    public index(List<String> fields, Boolean uni) {
+    public index(String db_name,String tb_name,List<String> fields, Boolean uni) {
+        this.db_name=db_name;
+        this.tb_name=tb_name;
         this.fields = fields;
         this.uni = uni;
     }
 
-    public index(@NotNull String fields, Boolean uni) {
+    public index(String db_ame,String tb_name,@NotNull String fields, Boolean uni) {
+        this.db_name=db_ame;
+        this.tb_name=tb_name;
         this.fields=Arrays.asList(fields.split(","));
         this.uni = uni;
     }
