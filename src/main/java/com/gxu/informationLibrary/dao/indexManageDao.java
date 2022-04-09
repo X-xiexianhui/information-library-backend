7b8cfb3fc740b6c10270cf99832c318209aba75e
xiexianhui <xiexianhuicn@outlook.com>
2022-04-09 16:13:31 +0800

    "alter index"

--- src/main/java/com/gxu/informationLibrary/dao/indexManageDao.java
+++ src/main/java/com/gxu/informationLibrary/dao/indexManageDao.java
@@ -2,6 +2,7 @@ package com.gxu.informationLibrary.dao;
 
 import com.gxu.informationLibrary.entity.index;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Repository
 public interface indexManageDao {
     void addIndex();
-    void dropIndex(String db_name,String tb_name,String index_name);
-    List<String>getColumns(String db_name,String tb_name);
-    List<index> getIndex(String db_name, String tb_name);
+    void dropIndex(@Param("db_name") String db_name,@Param("tb_name") String tb_name,@Param("index_name") String index_name);
+    List<String>getColumns(@Param("db_name") String db_name,@Param("tb_name") String tb_name);
+    List<index> getIndex(@Param("db_name") String db_name,@Param("tb_name") String tb_name);
 }
