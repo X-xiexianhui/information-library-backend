6f51f007fc161de1adc0312fe1215df3045969d9
xiexianhui <xiexianhuicn@outlook.com>
2022-04-10 17:03:59 +0800

    "alter index"

--- src/main/java/com/gxu/informationLibrary/entity/indexList.java
+++ src/main/java/com/gxu/informationLibrary/entity/indexList.java
@@ -13,13 +13,31 @@ import java.util.List;
 public class indexList {
     List<index>indexes=new ArrayList<>();
 
-    public indexList(@NotNull JSONArray indexArray) {
+    public indexList(@NotNull JSONArray indexArray,boolean isAdd) {
         for (int i = 0; i < indexArray.size(); i++) {
             JSONObject object=indexArray.getJSONObject(i);
-            String index_name= object.getString("index_name");
             List<String>fields= object.getJSONArray("fields").toJavaList(String.class);
-            boolean uni=object.getBoolean("uni");
+            boolean uni= object.getBoolean("uni");
+            String index_name;
+            if (isAdd){
+                index_name=object.getString("index_name");
+            }else {
+                index_name=getIndexName(fields,uni);
+            }
             indexes.add(new index(index_name,fields,uni));
         }
+    }
+    private String getIndexName(List<String>fields,boolean uni){
+        StringBuilder index_name= new StringBuilder();
+        for (String str:fields) {
+            index_name.append(str);
+            index_name.append("_");
+        }
+        if (uni){
+            index_name.append("uni_index");
+        }else {
+            index_name.append("index");
+        }
+        return index_name.toString();
     }
 }
