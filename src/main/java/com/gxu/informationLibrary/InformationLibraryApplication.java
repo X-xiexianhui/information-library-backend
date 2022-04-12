d5a9581e8a3f84114108040336f9d35d88ff3a41
xiexianhui <xiexianhuicn@outlook.com>
2022-04-12 23:51:33 +0800

    "alter fk"

--- src/main/java/com/gxu/informationLibrary/InformationLibraryApplication.java
+++ src/main/java/com/gxu/informationLibrary/InformationLibraryApplication.java
@@ -3,8 +3,6 @@ package com.gxu.informationLibrary;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
