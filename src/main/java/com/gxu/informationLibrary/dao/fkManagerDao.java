b6d5f39c604090de5195ddd6acf9317dc72f900e
xiexianhui <xiexianhuicn@outlook.com>
2022-04-12 16:29:34 +0800

    "alter fk"

--- src/main/java/com/gxu/informationLibrary/dao/fkManagerDao.java
+++ src/main/java/com/gxu/informationLibrary/dao/fkManagerDao.java
@@ -1,6 +1,14 @@
 package com.gxu.informationLibrary.dao;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface fkManagerDao {
-    void addFK();
-    void deleteFK();
+    void addFk();
+    void deleteFk();
+    void insertFkInfo();
+    void deleteFkInfo();
+    List<String>getRefTable();
+    List<String>getRefColumn(@Param("ref_table") String ref_table);
 }
