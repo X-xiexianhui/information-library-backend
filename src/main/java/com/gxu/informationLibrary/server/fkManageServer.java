c52f6defd8dd3c4bf03a8f4d162dc40c0cf83b8d
xiexianhui <xiexianhuicn@outlook.com>
2022-04-12 16:41:18 +0800

    "alter fk"

--- src/main/java/com/gxu/informationLibrary/server/fkManageServer.java
+++ src/main/java/com/gxu/informationLibrary/server/fkManageServer.java
@@ -5,8 +5,8 @@ import com.gxu.informationLibrary.entity.fk;
 import java.util.List;
 
 public interface fkManageServer {
-    void addFk();
-    void deleteFk();
-    void alterFk();
+    void addFk(String db_name,String tb_name,List<fk>insert);
+    void deleteFk(String db_name,String tb_name,List<fk>remove);
+    void alterFk(String db_name,String tb_name,List<fk>update);
     List<fk>getFk();
 }
