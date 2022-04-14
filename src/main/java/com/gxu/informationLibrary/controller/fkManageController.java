ef07b4dd83562b851bf620dde1f89dc9ce7bddaa
xiexianhui <xiexianhuicn@outlook.com>
2022-04-14 10:39:20 +0800

    "fk manager"

--- src/main/java/com/gxu/informationLibrary/controller/fkManageController.java
+++ src/main/java/com/gxu/informationLibrary/controller/fkManageController.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @ResponseBody
