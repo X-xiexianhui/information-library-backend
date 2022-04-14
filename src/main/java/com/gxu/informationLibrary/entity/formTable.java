ebde29561bf2bd7e5ed2b23343d6f38612a9339d
xiexianhui <xiexianhuicn@outlook.com>
2022-04-14 10:24:27 +0800

    "alter fk"

---
+++ src/main/java/com/gxu/informationLibrary/entity/formTable.java
@@ -0,0 +1,9 @@
+package com.gxu.informationLibrary.entity;
+
+import lombok.Data;
+
+@Data
+public class formTable {
+    String form_name;
+    String table;
+}
---
+++ src/main/java/com/gxu/informationLibrary/server/formManageServer.java
@@ -0,0 +1,8 @@
+package com.gxu.informationLibrary.server;
+
+public interface formManageServer {
+    void addForm();
+    void deleteForm();
+    void queryForm();
+    void editForm();
+}
