8674fb64529b4c8c63a4cf3bea324deb74641fd2
xiexianhui <xiexianhuicn@outlook.com>
2022-04-14 10:50:46 +0800

    "fk manager"

--- src/main/java/com/gxu/informationLibrary/dao/tableManagerDao.java
+++ src/main/java/com/gxu/informationLibrary/dao/tableManagerDao.java
@@ -29,23 +29,27 @@ public interface tableManagerDao {
     void dropColumn(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("col_name") String col_name);
 
     //    修改列名或者类型
-    void changeColumn(@Param("db_name")String db_name,@Param("tb_name") String tb_name,@Param("col_name") String col_name,@Param("new_column") String new_column,@Param("data_type") String data_type,@Param("len") int len,@Param("place") int place);
+    void changeColumn(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("col_name") String col_name, @Param("new_column") String new_column, @Param("data_type") String data_type, @Param("len") int len, @Param("place") int place);
 
-    void dropPK(@Param("db_name")String db_name,@Param("tb_name") String tb_name);
+    void dropPK(@Param("db_name") String db_name, @Param("tb_name") String tb_name);
 
-    void addPK(@Param("db_name") String db_name,@Param("tb_name") String tb_name,@Param("pks") List<String>pks);
+    void addPK(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("pks") List<String> pks);
 
-    void setNotNull(@Param("db_name")String db_name,@Param("tb_name") String tb_name,@Param("col_name") String col_name,@Param("data_type") String data_type,@Param("len") int len,@Param("place") int place,@Param("not_null")boolean not_null);
+    void setNotNull(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("col_name") String col_name, @Param("data_type") String data_type, @Param("len") int len, @Param("place") int place, @Param("not_null") boolean not_null);
 
-    void addUnique(@Param("db_name") String db_name,@Param("tb_name") String tb_name,@Param("col_name") String col_name);
+    void addUnique(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("col_name") String col_name);
 
-    void dropUnique(@Param("db_name") String db_name,@Param("tb_name") String tb_name,@Param("key_name") String key_name);
+    void dropUnique(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("key_name") String key_name);
 
     JSONObject showKeys(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("col_name") String col_name);
 
-    <T> List<String> query(@Param("query_col")String query_col,@Param("query_name")String query_name,@Param("query_value")T query_value);
-    <T>int queryInt(@Param("query_col")String query_col,@Param("query_name")String query_name,@Param("query_value")T query_value);
+    <T> List<String> query(@Param("query_col") String query_col, @Param("query_name") String query_name, @Param("query_value") T query_value);
+
+    <T> int queryInt(@Param("query_col") String query_col, @Param("query_name") String query_name, @Param("query_value") T query_value);
+
     <T> void setColumnInfo(@Param("col_name") String col_name, @Param("new_value") T new_value, @Param("col_id") int col_id);
-    List<String>getPKs(@Param("db_name")String db_name,@Param("tb_name") String tb_name);
-    int getPrimaryKey(@Param("db_name") String db_name,@Param("tb_name") String tb_name);
+
+    List<String> getPKs(@Param("db_name") String db_name, @Param("tb_name") String tb_name);
+
+    int getPrimaryKey(@Param("db_name") String db_name, @Param("tb_name") String tb_name);
 }
