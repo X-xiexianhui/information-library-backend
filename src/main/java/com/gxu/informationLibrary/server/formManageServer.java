4bd837cd1f4c64e2dc3a910e884613f8d84c9679
xiexianhui <xiexianhuicn@outlook.com>
2022-04-14 22:29:51 +0800

    "form manager"

--- src/main/java/com/gxu/informationLibrary/server/formManageServer.java
+++ src/main/java/com/gxu/informationLibrary/server/formManageServer.java
@@ -6,11 +6,11 @@ import com.gxu.informationLibrary.entity.formTable;
 import java.util.List;
 
 public interface formManageServer {
-    List<formStruct> addForm();
-
     List<formTable> deleteForm();
 
     List<formTable> queryForm();
+
+    List<formTable> renameForm();
 
     List<formStruct> editForm();
 }
