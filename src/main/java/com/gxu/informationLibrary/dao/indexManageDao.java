4862f3fb2a12d7837ca8a1deb21fac740e5e08fd
xiexianhui <xiexianhuicn@outlook.com>
2022-04-14 10:50:23 +0800

    "fk manager"

--- src/main/java/com/gxu/informationLibrary/dao/indexManageDao.java
+++ src/main/java/com/gxu/informationLibrary/dao/indexManageDao.java
@@ -6,11 +6,15 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Mapper
 @Repository
 public interface indexManageDao {
-    void addIndex(@Param("index_name") String index_name,String db_name,String tb_name,@Param("fields") List<String>fields,@Param("uni") boolean uni);
-    void dropIndex(@Param("db_name") String db_name,@Param("tb_name") String tb_name,@Param("index_name") String index_name);
-    List<String>getColumns(@Param("db_name") String db_name,@Param("tb_name") String tb_name);
-    List<index> getIndex(@Param("db_name") String db_name,@Param("tb_name") String tb_name);
+    void addIndex(@Param("index_name") String index_name, String db_name, String tb_name, @Param("fields") List<String> fields, @Param("uni") boolean uni);
+
+    void dropIndex(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("index_name") String index_name);
+
+    List<String> getColumns(@Param("db_name") String db_name, @Param("tb_name") String tb_name);
+
+    List<index> getIndex(@Param("db_name") String db_name, @Param("tb_name") String tb_name);
 }
