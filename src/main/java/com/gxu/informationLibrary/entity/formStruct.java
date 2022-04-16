6f0846c9cb03a8937d7801425353ed2558eab5ec
xiexianhui <xiexianhuicn@outlook.com>
2022-04-16 22:31:34 +0800

    "form manager"

--- src/main/java/com/gxu/informationLibrary/entity/formStruct.java
+++ src/main/java/com/gxu/informationLibrary/entity/formStruct.java
@@ -1,12 +1,8 @@
 package com.gxu.informationLibrary.entity;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
 public class formStruct {
     int form_id;
     String col_name;
