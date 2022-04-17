f43c228e844cfa666f93d0cddfd5fbc1b606f07c
xiexianhui <xiexianhuicn@outlook.com>
2022-04-17 11:16:06 +0800

    "data manager"

---
+++ src/main/java/com/gxu/informationLibrary/entity/dataRow.java
@@ -0,0 +1,11 @@
+package com.gxu.informationLibrary.entity;
+
+import lombok.Data;
+
+@Data
+public class dataRow {
+    String col_name;
+    String data_type;
+    String label_name;
+    String data;
+}
