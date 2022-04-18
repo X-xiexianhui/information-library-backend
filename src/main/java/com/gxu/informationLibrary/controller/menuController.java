8e9a4e3ceb738691f7bf0f4e903195fb31abff2a
xiexianhui <xiexianhuicn@outlook.com>
2022-04-18 21:26:53 +0800

    "menu"

--- src/main/java/com/gxu/informationLibrary/controller/menuController.java
+++ src/main/java/com/gxu/informationLibrary/controller/menuController.java
@@ -18,73 +18,80 @@ public class menuController {
     public menuController(menuImpl menu) {
         this.menu = menu;
     }
+
     @PostMapping("api/menu/add")
-    public response<List<menuInfo>>addMenu(@RequestBody String Param){
-        List<menuInfo>data=new ArrayList<>();
+    public response<List<menuInfo>> addMenu(@RequestBody String Param) {
+        List<menuInfo> data = new ArrayList<>();
         try {
-            data=menu.addMenu(Param);
-        }catch (Exception e){
-            return new response<>(500,e.getCause().getMessage(),data);
+            data = menu.addMenu(Param);
+        } catch (Exception e) {
+            return new response<>(500, e.getCause().getMessage(), data);
         }
         return new response<>(data);
     }
+
     @PostMapping("api/menu/delete")
-    public response<List<menuInfo>>deleteMenu(@RequestBody String Param){
-        List<menuInfo>data=new ArrayList<>();
+    public response<List<menuInfo>> deleteMenu(@RequestBody String Param) {
+        List<menuInfo> data = new ArrayList<>();
         try {
-            data=menu.deleteMenu(Param);
-        }catch (Exception e){
-            return new response<>(500,e.getCause().getMessage(),data);
+            data = menu.deleteMenu(Param);
+        } catch (Exception e) {
+            return new response<>(500, e.getCause().getMessage(), data);
         }
         return new response<>(data);
     }
+
     @GetMapping("api/menu/query")
-    public response<List<menuInfo>>query(@RequestParam("menu_name") String menu_name){
-        List<menuInfo>data=new ArrayList<>();
+    public response<List<menuInfo>> query(@RequestParam("menu_name") String menu_name) {
+        List<menuInfo> data = new ArrayList<>();
         try {
-            data=menu.queryMenu(menu_name);
-        }catch (Exception e){
-            return new response<>(500,e.getCause().getMessage(),data);
+            data = menu.queryMenu(menu_name);
+        } catch (Exception e) {
+            return new response<>(500, e.getCause().getMessage(), data);
         }
         return new response<>(data);
     }
+
     @PostMapping("api/menu/edit")
-    public response<List<menuInfo>>editMenu(@RequestBody String param){
-        List<menuInfo>data=new ArrayList<>();
+    public response<List<menuInfo>> editMenu(@RequestBody String param) {
+        List<menuInfo> data = new ArrayList<>();
         try {
-            data=menu.editMenu(param);
-        }catch (Exception e){
-            return new response<>(500,e.getCause().getMessage(),data);
+            data = menu.editMenu(param);
+        } catch (Exception e) {
+            return new response<>(500, e.getCause().getMessage(), data);
         }
         return new response<>(data);
     }
+
     @GetMapping("api/menu/select")
-    public response<List<Map<String,Object>>>getMenuSelect(){
-        List<Map<String,Object>>data=new ArrayList<>();
+    public response<List<Map<String, Object>>> getMenuSelect() {
+        List<Map<String, Object>> data = new ArrayList<>();
         try {
-            data=menu.queryMenu();
-        }catch (Exception e){
-            return new response<>(500,e.getCause().getMessage(),data);
+            data = menu.queryMenu();
+        } catch (Exception e) {
+            return new response<>(500, e.getCause().getMessage(), data);
         }
         return new response<>(data);
     }
+
     @GetMapping("api/menu/form")
-    public response<List<Map<String,Object>>>getFormSelect(){
-        List<Map<String,Object>>data=new ArrayList<>();
+    public response<List<Map<String, Object>>> getFormSelect() {
+        List<Map<String, Object>> data = new ArrayList<>();
         try {
-            data=menu.getFormSelect();
-        }catch (Exception e){
-            return new response<>(500,e.getCause().getMessage(),data);
+            data = menu.getFormSelect();
+        } catch (Exception e) {
+            return new response<>(500, e.getCause().getMessage(), data);
         }
         return new response<>(data);
     }
+
     @GetMapping("api/menu/id")
-    public response<menuInfo>getMenu(@RequestParam("form_id") int menu_id){
-        menuInfo data=new menuInfo();
+    public response<menuInfo> getMenu(@RequestParam("form_id") int menu_id) {
+        menuInfo data = new menuInfo();
         try {
-            data=menu.queryMenu(menu_id);
-        }catch (Exception e){
-            return new response<>(500,e.getCause().getMessage(),data);
+            data = menu.queryMenu(menu_id);
+        } catch (Exception e) {
+            return new response<>(500, e.getCause().getMessage(), data);
         }
         return new response<>(data);
     }
