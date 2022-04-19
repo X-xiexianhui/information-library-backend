10ea7162dca0be4355b69d01adee103b300fdd3a
xiexianhui <xiexianhuicn@outlook.com>
2022-04-19 10:55:13 +0800

    "role"

---
+++ src/main/java/com/gxu/informationLibrary/dao/roleDao.java
@@ -0,0 +1,26 @@
+package com.gxu.informationLibrary.dao;
+
+import com.gxu.informationLibrary.entity.roleInfo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+@Repository
+public interface roleDao {
+    void addRole(@Param("role") roleInfo role);
+
+    void  deleteRole(@Param("role_id") int role_id);
+
+    List<roleInfo>queryRole(@Param("role_name") String role_name);
+
+    List<roleInfo>queryById(@Param("role_id") int role_id);
+
+    List<Map<String,Object>>getRoleSelect();
+
+    void editRole(@Param("col_name") String col_name,@Param("value") String value);
+}
---
+++ src/main/java/com/gxu/informationLibrary/entity/roleInfo.java
@@ -0,0 +1,10 @@
+package com.gxu.informationLibrary.entity;
+
+import lombok.Data;
+
+@Data
+public class roleInfo {
+    int role_id;
+    String Role_name;
+    String description;
+}
