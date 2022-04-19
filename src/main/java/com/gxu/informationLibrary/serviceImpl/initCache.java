09bef9535f13a2df146a8cc7e200f828950e48d8
xiexianhui <xiexianhuicn@outlook.com>
2022-04-19 22:30:14 +0800

    "auth"

--- src/main/java/com/gxu/informationLibrary/serviceImpl/initCache.java
+++ src/main/java/com/gxu/informationLibrary/serviceImpl/initCache.java
@@ -1,23 +1,38 @@
 package com.gxu.informationLibrary.serviceImpl;
 
+import com.gxu.informationLibrary.dao.authDao;
+import com.gxu.informationLibrary.entity.roleAuth;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Slf4j
 @Service
 public class initCache {
-    private final RedisTemplate redisTemplate;
+    private final StringRedisTemplate redisTemplate;
+    private final authDao auth;
 
-    public initCache(RedisTemplate redisTemplate) {
+    public initCache(StringRedisTemplate redisTemplate, authDao auth) {
         this.redisTemplate = redisTemplate;
+        this.auth = auth;
     }
 
 
     @PostConstruct
     public void initCacheFunc(){
+        List<roleAuth>cache=auth.getAuth();
+        HashOperations<String,String,String> hashOps = redisTemplate.opsForHash();
+        for (roleAuth r:cache) {
+            String key="auth_"+r.getRole_id()+"_"+r.getForm_id();
+            hashOps.put(key,"add",r.getAdd());
+            hashOps.put(key,"del",r.getDel());
+            hashOps.put(key,"search",r.getSearch());
+            hashOps.put(key,"edit",r.getEdit());
+        }
         log.info("Redis初始化成功");
     }
 }
