58a03463b5d202a030c28ac6eedfac6c4aafe263
xiexianhui <xiexianhuicn@outlook.com>
2022-04-19 21:21:47 +0800

    "redis"

--- src/main/java/com/gxu/informationLibrary/InformationLibraryApplication.java
+++ src/main/java/com/gxu/informationLibrary/InformationLibraryApplication.java
@@ -2,10 +2,12 @@ package com.gxu.informationLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableCaching
 public class InformationLibraryApplication {
 
 	public static void main(String[] args) {
