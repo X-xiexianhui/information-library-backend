466afa26322aa9060525e56b28718feff37f99a1
xiexianhui <xiexianhuicn@outlook.com>
2022-04-22 15:42:06 +0800

    "user"

---
+++ src/main/java/com/gxu/informationLibrary/serviceImpl/userInfo.java
@@ -0,0 +1,12 @@
+package com.gxu.informationLibrary.serviceImpl;
+
+import lombok.Data;
+
+@Data
+public class userInfo {
+    String user_id;
+    String user_name;
+    String pwd;
+    String user_email;
+    String user_role;
+}
