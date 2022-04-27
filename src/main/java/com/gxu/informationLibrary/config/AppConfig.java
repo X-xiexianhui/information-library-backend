3334b2e148b799ee93ad2a89df0aac44f034d52e
xiexianhui <xiexianhuicn@outlook.com>
2022-04-27 19:53:03 +0800

    "login"

--- src/main/java/com/gxu/informationLibrary/config/AppConfig.java
+++ src/main/java/com/gxu/informationLibrary/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,16 +13,25 @@ import java.util.List;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-//    @Bean
-//    public CookieAndSessionInterceptor interceptor(){
-//        return new CookieAndSessionInterceptor();
-//    }
-//    @Override
-//    public void addInterceptors(@NotNull InterceptorRegistry registry) {
-//        List<String>pattern=new ArrayList<>();
-//        pattern.add("api/user/login");
-//        pattern.add("api/user/logout");
-//        pattern.add("api/pwd/reset");
-//        registry.addInterceptor(interceptor()).excludePathPatterns();
-//    }
+    @Bean
+    public CookieAndSessionInterceptor interceptor(){
+        return new CookieAndSessionInterceptor();
+    }
+    @Override
+    public void addInterceptors(@NotNull InterceptorRegistry registry) {
+        List<String>pattern=new ArrayList<>();
+        pattern.add("api/user/login");
+        pattern.add("api/user/logout");
+        pattern.add("api/pwd/reset");
+        registry.addInterceptor(interceptor()).excludePathPatterns();
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
 }
