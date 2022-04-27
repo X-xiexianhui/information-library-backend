88677a22adc5554a4943cf5038dfe4cf019ae306
xiexianhui <xiexianhuicn@outlook.com>
2022-04-27 19:54:49 +0800

    "login"

--- src/main/java/com/gxu/informationLibrary/config/AppConfig.java
+++ src/main/java/com/gxu/informationLibrary/config/AppConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
