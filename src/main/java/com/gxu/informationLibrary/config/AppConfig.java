81544dedc662fea813988b0c52cd5b1266c3e935
xiexianhui <xiexianhuicn@outlook.com>
2022-04-27 20:02:22 +0800

    "login"

--- src/main/java/com/gxu/informationLibrary/config/AppConfig.java
+++ src/main/java/com/gxu/informationLibrary/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.gxu.informationLibrary.config;
 
 import org.jetbrains.annotations.NotNull;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,17 +14,16 @@ import java.util.List;
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
     @Bean
-    public CookieAndSessionInterceptor interceptor(){
+    public CookieAndSessionInterceptor interceptor() {
         return new CookieAndSessionInterceptor();
     }
+
     @Override
     public void addInterceptors(@NotNull InterceptorRegistry registry) {
-        List<String>pattern= Arrays.asList("/", "css/**", "js/**", "img/**", "json/**", "fonts/**","/*.html");
-//        pattern.add("api/user/login");
-//        pattern.add("api/user/logout");
-//        pattern.add("api/pwd/reset");
+        List<String> pattern = Arrays.asList("/", "css/**", "js/**", "img/**", "json/**", "fonts/**", "/*.html", "api/user/login", "api/user/logout","api/pwd/reset");
         registry.addInterceptor(interceptor()).excludePathPatterns(pattern);
     }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
@@ -35,6 +33,7 @@ public class AppConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .maxAge(3600);
     }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**");
