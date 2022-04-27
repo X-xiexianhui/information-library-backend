70559ebf56dc37753cc11dccb8a00d3f30c3c031
xiexianhui <xiexianhuicn@outlook.com>
2022-04-27 14:44:22 +0800

    "login"

--- src/main/java/com/gxu/informationLibrary/config/AppConfig.java
+++ src/main/java/com/gxu/informationLibrary/config/AppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
     @Bean
@@ -14,6 +18,10 @@ public class AppConfig implements WebMvcConfigurer {
     }
     @Override
     public void addInterceptors(@NotNull InterceptorRegistry registry) {
+        List<String>pattern=new ArrayList<>();
+        pattern.add("api/user/login");
+        pattern.add("api/user/logout");
+        pattern.add("api/pwd/reset");
         registry.addInterceptor(interceptor()).excludePathPatterns();
     }
 }
