3c015010dd87ad3c9b8edef026cfb1a544ed2108
xiexianhui <xiexianhuicn@outlook.com>
2022-04-27 20:02:36 +0800

    "login"

--- src/main/java/com/gxu/informationLibrary/config/AppConfig.java
+++ src/main/java/com/gxu/informationLibrary/config/AppConfig.java
@@ -20,7 +20,7 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(@NotNull InterceptorRegistry registry) {
-        List<String> pattern = Arrays.asList("/", "css/**", "js/**", "img/**", "json/**", "fonts/**", "/*.html", "api/user/login", "api/user/logout","api/pwd/reset");
+        List<String> pattern = Arrays.asList("/", "css/**", "js/**", "img/**", "json/**", "fonts/**", "/*.html", "api/user/login", "api/user/logout", "api/pwd/reset");
         registry.addInterceptor(interceptor()).excludePathPatterns(pattern);
     }
 
