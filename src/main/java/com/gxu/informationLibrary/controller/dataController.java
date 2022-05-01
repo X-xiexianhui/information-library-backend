07975d83f0e0c8903a52a2f9bd4c40da829d7d7c
xiexianhui <xiexianhuicn@outlook.com>
2022-05-01 16:29:37 +0800

    "data"

--- src/main/java/com/gxu/informationLibrary/controller/dataController.java
+++ src/main/java/com/gxu/informationLibrary/controller/dataController.java
@@ -18,24 +18,29 @@ public class dataController {
     public dataController(dataImpl dataManage) {
         this.dataManage = dataManage;
     }
+
     @GetMapping("api/data/column")
-    public response<List<Map<String,String>>>getTableColumn(@RequestParam("form_id")int form_id){
+    public response<List<Map<String, String>>> getTableColumn(@RequestParam("form_id") int form_id) {
         return dataManage.getTableColumn(form_id);
     }
+
     @GetMapping("api/data/get")
-    public response<List<JSONObject>>getTableData(@RequestParam("form_id")int form_id){
+    public response<List<JSONObject>> getTableData(@RequestParam("form_id") int form_id) {
         return dataManage.getTableData(form_id);
     }
+
     @PostMapping("api/data/add")
-    public response<String>insertData(@RequestBody String parma){
+    public response<String> insertData(@RequestBody String parma) {
         return dataManage.insertData(parma);
     }
+
     @PostMapping("api/data/edit")
-    public response<String>editData(@RequestBody String parma){
+    public response<String> editData(@RequestBody String parma) {
         return dataManage.updateData(parma);
     }
+
     @PostMapping("api/file/add")
-    public response<String>uploadFile(@RequestParam("file") MultipartFile file){
+    public response<String> uploadFile(@RequestParam("file") MultipartFile file) {
         return dataManage.uploadFile(file);
     }
 }
