05adb2790912d8d05041ba7c22232e594e290fe2
xiexianhui <xiexianhuicn@outlook.com>
2022-05-03 15:01:12 +0800

    "data"

--- src/main/java/com/gxu/informationLibrary/dao/dataManageDao.java
+++ src/main/java/com/gxu/informationLibrary/dao/dataManageDao.java
@@ -13,17 +13,17 @@ import java.util.Map;
 @Mapper
 @Repository
 public interface dataManageDao {
-    void insertData(@Param("db_name") String db_name,@Param("tb_name") String tb_name,List<editEntity>columns);
+    void insertData(@Param("db_name") String db_name, @Param("tb_name") String tb_name, List<editEntity> columns);
 
-    void updateData(String db_name,String tb_name,int record_id,List<editEntity>updates);
+    void updateData(String db_name, String tb_name, int record_id, List<editEntity> updates);
 
-    void deleteData(@Param("db_name") String db_name,@Param("tb_name") String tb_name,@Param("record_id") int record_id);
+    void deleteData(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("record_id") int record_id);
 
-    List<JSONObject>queryData(@Param("db_name") String db_name,@Param("tb_name") String tb_name,@Param("columns") List<editEntity>columns, @Param("onlyAuth") boolean onlyAuth,@Param("user_id") String user_id);
+    List<JSONObject> queryData(@Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("columns") List<editEntity> columns, @Param("onlyAuth") boolean onlyAuth, @Param("user_id") String user_id);
 
-    List<Map<String,String>>getTableColumn(@Param("form_id") int form_id);
+    List<Map<String, String>> getTableColumn(@Param("form_id") int form_id);
 
-    Map<String,String> getTableByFormId(@Param("form_id") int form_id);
+    Map<String, String> getTableByFormId(@Param("form_id") int form_id);
 
-    statisticsResult statistics(@Param("option") String option,@Param("db_name")String db_name,@Param("tb_name") String tb_name,@Param("field") String field,@Param("group_field") String group_field,@Param("onlyUser") boolean onlyUser,String user_id);
+    statisticsResult statistics(@Param("option") String option, @Param("db_name") String db_name, @Param("tb_name") String tb_name, @Param("field") String field, @Param("group_field") String group_field, @Param("onlyUser") boolean onlyUser, @Param("user_id") String user_id);
 }
