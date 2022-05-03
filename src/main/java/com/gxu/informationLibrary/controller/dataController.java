4110a41cf9edcc0642312fd5034a3ff7159fa7e4
xiexianhui <xiexianhuicn@outlook.com>
2022-05-03 14:52:31 +0800

    "data"

--- src/main/java/com/gxu/informationLibrary/controller/dataController.java
+++ src/main/java/com/gxu/informationLibrary/controller/dataController.java
@@ -49,8 +49,9 @@ public class dataController {
     public response<String> uploadFile(@RequestParam("file") MultipartFile file) {
         return dataManage.uploadFile(file);
     }
+
     @PostMapping("api/data/dump")
-    public String dumpData(){
+    public String dumpData() {
         return dataManage.dumpData();
     }
 }
