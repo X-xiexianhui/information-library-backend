da99a10a1ddcda032ffb6b9bb3ce92828e9d63cc
xiexianhui <xiexianhuicn@outlook.com>
2022-05-03 11:52:04 +0800

    "dataBaseDump"

---
+++ src/main/java/com/gxu/informationLibrary/entity/statisticsResult.java
@@ -0,0 +1,10 @@
+package com.gxu.informationLibrary.entity;
+
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class statisticsResult {
+    List<Integer>result;
+    List<String>col_name;
+}
