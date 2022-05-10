b558fea0fb14f7345072d89bb896cf7aed547579
xiexianhui <xiexianhuicn@outlook.com>
2022-05-10 16:21:14 +0800

    "data"

--- src/main/java/com/gxu/informationLibrary/config/GlobalCorsConfig.java
+++ src/main/java/com/gxu/informationLibrary/config/GlobalCorsConfig.java
@@ -1,11 +1,10 @@
 package com.gxu.informationLibrary.config;
-//合并打包后，不需要跨域过滤器，否则cookie会失效
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-//@Configuration
+@Configuration
 public class GlobalCorsConfig {
     @Bean
     public CorsFilter corsFilter() {
