47d5109d1e5876d1645b6f1fea0a03d6e8cd74f6
xiexianhui <xiexianhuicn@outlook.com>
2022-05-11 09:30:55 +0800

    "data"

---
+++ src/main/java/com/gxu/informationLibrary/entity/recycleBin.java
@@ -0,0 +1,22 @@
+package com.gxu.informationLibrary.entity;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+
+@Data
+public class recycleBin {
+    int id;
+    int form_id;
+    JSONObject data;
+    String user;
+    String del_time;
+
+    public recycleBin(int id, int form_id, String data, String user, String del_time) {
+        this.id = id;
+        this.form_id = form_id;
+        this.data = JSON.parseObject(data);
+        this.user = user;
+        this.del_time = del_time;
+    }
+}
