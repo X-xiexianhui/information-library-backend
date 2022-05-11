73cb1c115c1de3d5b49535902fd3475a78ee6459
xiexianhui <xiexianhuicn@outlook.com>
2022-05-11 16:53:56 +0800

    "data"

--- src/main/java/com/gxu/informationLibrary/serviceImpl/dataImpl.java
+++ src/main/java/com/gxu/informationLibrary/serviceImpl/dataImpl.java
@@ -232,7 +232,7 @@ public class dataImpl implements dataServer {
                 columns.add(c);
             }
             dataManage.insertData(tb.get("db_name"), tb.get("tb_name"), columns);
-            dataManage.deleteRecycleData(dataJSON.getIntValue("id"));
+            dataManage.deleteRecycleData(dataJSON.getIntValue("del_id"));
         } catch (Exception e) {
             return new response<>(500, e.getCause().getMessage(), false);
         }
@@ -253,7 +253,7 @@ public class dataImpl implements dataServer {
                     columns.add(c);
                 }
                 dataManage.insertData(tb.get("db_name"), tb.get("tb_name"), columns);
-                dataManage.deleteRecycleData(d.getIntValue("id"));
+                dataManage.deleteRecycleData(d.getIntValue("del_id"));
             }
         }catch (Exception e){
             return new response<>(500,e.getCause().getMessage(),false);
